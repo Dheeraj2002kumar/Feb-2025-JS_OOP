75ca4d939b9aec52d2d633f1bf1c4a39add27680
Dheeraj Kumar <dk8549644@gmail.com>
2025-03-03 19:32:12 +0530

    Add constructor functions for Tea and Animal, implement prototype methods, and add error handling for Drink function

--- Functional_constructor_and_error/functional_constructor_and_error.js
+++ Functional_constructor_and_error/functional_constructor_and_error.js
@@ -20,3 +20,49 @@ let myNewCar = new Car("Tata", "Safari");
 // console.log(myNewCar);
 
 
+// constructor function and function name should be capital (first character)
+function Tea(type){
+    this.type = type;
+    this.describe = function (){
+        return `this is a cup of ${this.type}`;
+    };
+}
+
+let lemonTea = new Tea("lemon tea");
+// console.log(lemonTea);
+// console.log(lemonTea.describe());
+
+
+
+// constructor funciton and prototype --------------------
+
+function Animal(species){
+    this.species = species;
+}
+
+Animal.prototype.sound = function(){
+    return `${this.species} make a sound`;
+}
+
+let dog = new Animal("Dog");
+// console.log(dog.sound());
+
+let Cat = new Animal("Cat");
+// console.log(Cat.sound());
+
+
+
+// error check when Drink function called without new keyword 
+function Drink(name){
+    if (!new.target){
+        throw new Error("Drink mus be called with new keyword");        
+    }
+
+    this.name = name;
+}
+
+let tea = new Drink("tea");
+let coffee = Drink("coffee");  // without new keyword
+
+console.log(tea)
+console.log(coffee);
