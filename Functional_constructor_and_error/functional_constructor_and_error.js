d02d99d450bc1b7d1512ffcb657039c0def6e581
Dheeraj Kumar <dk8549644@gmail.com>
2025-03-03 19:06:45 +0530

    Add constructor functions for Person and Car with object creation examples

--- Functional_constructor_and_error/functional_constructor_and_error.js
+++ Functional_constructor_and_error/functional_constructor_and_error.js
@@ -1,1 +1,22 @@
-console.log("functional_constructor_and_error");
+// console.log("functional_constructor_and_error");
+
+// constructor function 
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+}
+
+// constructor function 
+function Car(make, model){
+    this.make = make;
+    this.model = model;
+}
+
+// Object creation
+let myCar = new Car("Toyota", "Camry");
+// console.log(myCar);
+
+let myNewCar = new Car("Tata", "Safari");
+// console.log(myNewCar);
+
+
