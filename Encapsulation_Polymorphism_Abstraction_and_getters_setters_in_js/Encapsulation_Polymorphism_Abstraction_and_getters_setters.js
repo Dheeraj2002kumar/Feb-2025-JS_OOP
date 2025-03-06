5ed452bd783549a68ec8933092c153f8023f8b15
Dheeraj Kumar <dk8549644@gmail.com>
2025-03-06 09:56:24 +0530

    Refactor JavaScript demonstration: improve formatting, add comments for private fields, and enhance class structure for encapsulation, abstraction, and polymorphism

--- Encapsulation_Polymorphism_Abstraction_and_getters_setters_in_js/Encapsulation_Polymorphism_Abstraction_and_getters_setters.js
+++ Encapsulation_Polymorphism_Abstraction_and_getters_setters_in_js/Encapsulation_Polymorphism_Abstraction_and_getters_setters.js
@@ -2,18 +2,18 @@ console.log("Output => \n");
 
 // Encapsulation_Polymorphism_Abstraction_and_getters_setters_in_js
 
-
+// ( # ) Hash Symbol is used for private
 
 // -------------------- Encapsulation ----------------------
-class BankAccount{
+class BankAccount {
     #balance = 0; // # symbol for hidding the data like balance
 
-    deposit(amount){
+    deposit(amount) {
         this.#balance += amount;
         return this.#balance;
     }
 
-    getBalance(){
+    getBalance() {
         return `$ ${this.#balance}`;
     }
 }
@@ -29,20 +29,20 @@ let account = new BankAccount();
 
 
 // --------------- Abstraction ------------------------
-class CoffeMachine{
-    start(){
+class CoffeMachine {
+    start() {
         // call DB
         // filter val
         return `Stating the machine...`
     }
 
-    brewCoffe(){
+    brewCoffe() {
         //complext calculation
         return `Brewing coffee`;
     }
 
     // wrap in one method called Abstraction
-    pressStartButton(){
+    pressStartButton() {
         let messageOne = this.start();
         let messageTwo = this.brewCoffe();
         return `${messageOne} \n ${messageTwo}`;
@@ -54,3 +54,53 @@ let myMachine = new CoffeMachine();
 
 // console.log(myMachine.start());
 // console.log(myMachine.brewCoffe());
+
+
+
+
+
+//  -------------------- Polymorphism ----------------------
+
+class Bird {
+    fly() {
+        return `Flying...`;
+    }
+}
+
+class Penguin extends Bird {
+    fly() {
+        return `Penguins can't fly...`;
+    }
+}
+
+let bird = new Bird();
+let penguin = new Penguin();
+
+// console.log(bird.fly());
+// console.log(penguin.fly());
+
+
+
+
+
+// Static ----------------------------
+class Calculator {
+    // Static method
+    static add(a, b) {
+        return a + b;
+    }
+
+    // Instance method
+    subtract(a, b) {
+        return a - b;
+    }
+}
+
+// Calling the static method directly on the class
+console.log(Calculator.add(5, 3)); // Output: 8
+
+// Creating an instance of the class
+const calc = new Calculator();
+
+// Calling the instance method on the instance
+console.log(calc.subtract(5, 3)); // Output: 2
